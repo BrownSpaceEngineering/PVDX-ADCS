0429d2aba14196ff50371a13c244f433528ec74e
david-man <86388290+david-man@users.noreply.github.com>
2026-03-18 12:00:18 -0400

    bdot

--- MATLAB/Algorithms/simulate_1vec.m
+++ MATLAB/Algorithms/simulate_1vec.m
@@ -6,7 +6,12 @@ function simulate_1vec()
     true_body_to_ref = Quaternion([0.2812, -0.6998, 0.5497, -0.3592]).quaternion_normalize();%the true body --> reference rotation
     current_guess = true_body_to_ref;%the estimated body --> reference rotation
 
-    true_omega = deg2rad([1, 0.05, -1]);%true angular velocity in the reference frame
+    true_omega = deg2rad([2, 0.5, -1]);%true angular velocity in the reference frame
+
+    %in Simulink, you need to take the omega_icrf2b and do
+    %omega_b = true_ref_to_body.apply_rotation(-omega_icrf2b);
+    %true_omega = reshape(deg2rad(omega_b), [1,3]);
+    
     gyro_bias = [0.001, -0.001, 0.001];
     gyro_noise = 0.001;
     measurement_noise = 0.03;
@@ -14,14 +19,14 @@ function simulate_1vec()
     state = zeros([1,6]);%starting state for the filter
     cov = eye(6) * 0.1;%starting covariance for the filter
     Q = eye(6) * 0.01;%process noise matrix
-    R = eye(6) * 5;%measurement noise matrix for 1 vector
+    R = diag([0.03, 0.03, 0.03, 1, 1, 1]);%measurement noise matrix for 1 vector
 
-    reference = [60, -60, 30];
+    reference = [40, -40, 30];
     last_reference = reference;
     reference_to_body = true_body_to_ref.quaternion_inverse();
     last_body = reference_to_body.apply_rotation(last_reference);
     err = 0;
-    total_err = [0 ,0, 0];
+    total_abs_err = [0 ,0, 0];
     for i = 1:1000 %iterate
         simulated_gyro_measurement = true_omega + normrnd(gyro_bias, gyro_noise);
 
@@ -53,7 +58,7 @@ function simulate_1vec()
         state(1:6) = zeros([1,6]);%for 1 vector --> no bias estimation
         last_body = body;
         last_reference = reference;
-        reference = reference + normrnd(0, 0.05, [1, 3]);
+        reference = reference + normrnd(0, 0.001, [1, 3]);
         if mod(i, 50) == 0
             disp("Iteration");
             %disp(current_guess)
@@ -62,12 +67,12 @@ function simulate_1vec()
             
         end
         err = err + norm(rad2deg(Quaternion.quat_diff(true_body_to_ref, current_guess)));
-        total_err = total_err + rad2deg(Quaternion.quat_diff(true_body_to_ref, current_guess));
+        total_abs_err = total_abs_err + abs(rad2deg(Quaternion.quat_diff(true_body_to_ref, current_guess)));
         
         
     end
     disp(err / 1000);
-    disp(total_err / 1000);
+    disp(total_abs_err / 1000);
     
 end
 
